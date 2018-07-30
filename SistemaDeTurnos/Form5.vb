5a22a18c6824284e8739517b2a25383a91632687
Keren Ramirez <kerenr450@gmail.com>
2018-07-29 20:17:09 -0600

    Todos los formularios son visibles en el PanelContenedor

--- SistemaDeTurnos/Form5.vb
+++ SistemaDeTurnos/Form5.vb
@@ -2,6 +2,8 @@
 
 Public Class Form5
     Public form1 As New Form1()
+    Public form4 As New Form4()
+    Public form3 As New Form3()
 
     '<DllImport("user32.DLL", EntryPoint:="ReleaseCapture")>
     Private Shared Sub ReleaseCapture()
@@ -58,16 +60,44 @@ Public Class Form5
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         If form1.Visible = False Then form1.Visible = True Else form1.Visible = False
-
     End Sub
 
     Private Sub Form5_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'configuranciones de form1 (entrada)
         form1.TopLevel = False
         form1.FormBorderStyle = FormBorderStyle.FixedSingle
         form1.Size = New System.Drawing.Size(250, 250)
         form1.Location = New System.Drawing.Point(20, 20)
         form1.WindowState = FormWindowState.Normal
         form1.Visible = False
+
+        'configuranciones de form4 (pantallas)
+        form4.TopLevel = False
+        form4.FormBorderStyle = FormBorderStyle.FixedSingle
+        form4.Size = New System.Drawing.Size(550, 400)
+        form4.Location = New System.Drawing.Point(20, 20)
+        form4.WindowState = FormWindowState.Normal
+        form4.Visible = False
+
+        'configuranciones de form3 (ventanilla)
+        form3.TopLevel = False
+        form3.FormBorderStyle = FormBorderStyle.FixedSingle
+        form3.Size = New System.Drawing.Size(750, 400)
+        form3.Location = New System.Drawing.Point(20, 20)
+        form3.WindowState = FormWindowState.Normal
+        form3.Visible = False
+
+        'agregar los formularios a el contenedor
         PanelContenedor.Controls.Add(form1)
+        PanelContenedor.Controls.Add(form4)
+        PanelContenedor.Controls.Add(form3)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If form4.Visible = False Then form4.Visible = True Else form4.Visible = False
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If form3.Visible = False Then form3.Visible = True Else form3.Visible = False
     End Sub
 End Class
